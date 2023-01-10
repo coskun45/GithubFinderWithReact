d065a3837ebc973fb33c4608eaf26ca39696bc1c
Coskun, Eyuep <eyuep.coskun@adesso.de>
2023-01-10 15:30:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import User from "./components/User";
+import Users from "./components/Users";
+import Search from "./components/Search";
+import Alert from "./components/Alert";
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  constructor(props) {
+      super(props);
+      this.searchUsers = this.searchUsers.bind(this);
+      this.clearUsers = this.clearUsers.bind(this);
+      this.setAlert = this.setAlert.bind(this);
+      this.state = {
+          loading: false,
+          users: [],
+          alert:null
+      }
+  }
+
+  searchUsers(keyword) {
+      this.setState({loading: true});
+      setTimeout(() => {
+          axios
+              .get(`https://api.github.com/search/users?q=${keyword}`)
+              .then(res => this.setState({users: res.data.items, loading: false}));
+      }, 1000);
+      
+  }
+  clearUsers(){
+    this.setState({ users: [] })
+
+  }
+  setAlert(msg,type){
+    this.setState({alert:{msg,type}});
+
+    setTimeout(()=>{
+      this.setState({alert:null});
+    },3000)
+  }
+
+  render() {
+      return (
+          <>
+              <Navbar />
+              <Alert alert={this.state.alert}/>
+              <Search 
+                searchUsers={this.searchUsers} 
+                clearUsers={this.clearUsers}
+                showClearButton={this.state.users.length >0 ?true :false} 
+                setAlert={this.setAlert}
+                />
+              <Users users={this.state.users} loading={this.state.loading}/>
+          </>
+      )
+  }
 }
 
-export default App;
+export default App
+
